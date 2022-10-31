0fe7ecd179a7ecffc5e5a394fc3c37b812e96af4
John Mixon-Sayah <johnmixonsayah@gmail.com>
2022-10-31 16:49:22 -0400

    oops

--- music.py
+++ music.py
@@ -1,19 +1,22 @@
 import sys
 import os
-
+import pdb
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import patches
-import pandas as pd
 
 
-sys.path.append('..')
 
-fn = os.path.join('FMP_C1_F15_Eflat.xml')
+def main():
+    sys.path.append('..')
 
-with open(fn, 'r') as stream:
-    xml_str = stream.read()
+    fn = os.path.join('FMP_C1_F15_Eflat.xml')
 
-start = xml_str.find('<note')
-end = xml_str[start:].find('</note>') + start + len('</note>')
-print(xml_str[start:end])
+    with open(fn, 'r') as stream:
+        xml_str = stream.read()
+
+    start = xml_str.find('<note')
+    end = xml_str[start:].find('</note>') + start + len('</note>')
+    print(xml_str[start:end])
+    pdb.set_trace()
+if __name__ == '__main__':
+    main()
