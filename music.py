f7b617848a5364550783fc70cb32cc26952603a6
John Mixon-Sayah <johnmixonsayah@gmail.com>
2022-11-01 15:30:01 -0400

    Update music.py

--- music.py
+++ music.py
@@ -3,13 +3,13 @@ import os
 import pdb
 import numpy as np
 from matplotlib import pyplot as plt
-
+import xml.etree.ElementTree as ET
 
 
 def main():
     sys.path.append('..')
 
-    fn = os.path.join('FMP_C1_F15_Eflat.xml')
+    fn = os.path.join('Untitled.musicxml')
 
     with open(fn, 'r') as stream:
         xml_str = stream.read()
@@ -17,6 +17,8 @@ def main():
     start = xml_str.find('<note')
     end = xml_str[start:].find('</note>') + start + len('</note>')
     print(xml_str[start:end])
+    # tree = ET.parse(fn)
+    # root = tree.getroot()
     pdb.set_trace()
 if __name__ == '__main__':
     main()
