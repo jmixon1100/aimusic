f0dd49c617205460c1c6ff6e01bd0533540bc280
John Mixon-Sayah <johnmixonsayah@gmail.com>
2022-11-03 15:37:15 -0400

    got the import to work

--- music.py
+++ music.py
@@ -1,24 +1,34 @@
-import sys
 import os
 import pdb
 import numpy as np
 from matplotlib import pyplot as plt
-import xml.etree.ElementTree as ET
+from lxml import etree
+import argparse
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+parser = argparse.ArgumentParser(
+    description="Train a perceptron to classify letters.")
+parser.add_argument('-x', '--data',
+                    help='path to data file, defaults to ROOT/data.txt',
+                    default=os.path.join(ROOT, 'untitled.musicxml'))
 
 
-def main():
-    sys.path.append('..')
-
-    fn = os.path.join('Untitled.musicxml')
-
-    with open(fn, 'r') as stream:
-        xml_str = stream.read()
-
-    start = xml_str.find('<note')
-    end = xml_str[start:].find('</note>') + start + len('</note>')
-    print(xml_str[start:end])
-    # tree = ET.parse(fn)
-    # root = tree.getroot()
+def main(args):
+    datafile = os.path.expanduser(args.data)
+    notes = []
+    accidentals = []
+    # this section of code took way too long
+    xml_str = etree.parse(datafile)
+    for pitch in xml_str.xpath('//pitch'):
+        notes.append(pitch.xpath(".//step/text()")[0])
+        accidentals.append(pitch.xpath(".//alter/text()")
+                           [0] if pitch.xpath(".//alter/text()") != [] else '0')
     pdb.set_trace()
+
+
+def encode_notes():
+    tones = {'A': 1, 'B': 4, 'C': 7, 'D': 10, 'E': 13, 'F': 16, 'G': 19}
+
+
 if __name__ == '__main__':
-    main()
+    main(parser.parse_args())
