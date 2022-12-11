313f52b4213305ba15b4f5452fc625fc21555986
John Mixon-Sayah <johnmixonsayah@gmail.com>
2022-12-11 15:38:51 -0500

    oops

--- naivebayes_solution copy.py
+++ naivebayes_solution copy.py
@@ -7,7 +7,6 @@ import pdb
 import numpy as np
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from sklearn.naive_bayes import GaussianNB
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -62,9 +61,9 @@ def main(args):
 
     # Change indexing to 0
     # xtrain[:, 0:2] -= 1
-    # ytrain += 7
+    ytrain += 7
     # xtest[:, 0:2] -= 1
-    # ytest += 7
+    ytest += 7
 
     # Extract useful parameters
     num_training_documents = len(ytrain)
@@ -72,10 +71,59 @@ def main(args):
     num_words = len(notes)
     num_newsgroups = len(key_sigs)
 
-    clf = GaussianNB()
-    clf.fit(xtrain, ytrain)
+    print("\n=======================")
+    print("TRAINING")
+    print("=======================")
 
-    print(clf.predict([xtest[1,:]]))
+    # Estimate the prior probabilities
+    print("Estimating prior probabilities via MLE...")
+    priors = np.bincount(ytrain) / num_training_documents
+    pdb.set_trace()
+    # Estimate the class conditional probabilities
+    print("Estimating class conditional probabilities via MAP...")
+    class_conditionals = np.zeros((num_words, num_newsgroups))
+    rows = xtrain[:, 1].tolist()
+    cols = ytrain[xtrain[:, 0]].tolist()
+
+    np.add.at(class_conditionals, (rows, cols), xtrain[:, 2])
+    alpha = (1 / num_words)
+    class_conditionals += alpha
+    class_conditionals /= np.sum(class_conditionals, 0)
+    # pdb.set_trace()
+    print("\n=======================")
+    print("TESTING")
+    print("=======================")
+
+    # Test the Naive Bayes classifier
+    print("Applying natural log to prevent underflow...")
+    log_priors = np.log(priors)
+    log_class_conditionals = np.log(class_conditionals)
+
+    print("Counting words in each document...")
+    counts = np.zeros((num_testing_documents, num_words))
+    rows = xtest[:, 0].tolist()
+    cols = xtest[:, 1].tolist()
+    np.add.at(counts, (rows, cols), xtest[:, 2])
+
+    print("Computing posterior probabilities...")
+    log_posterior = np.matmul(counts, log_class_conditionals)
+    log_posterior += log_priors
+
+    print("Assigning predictions via argmax...")
+    pred = np.argmax(log_posterior, 1)
+
+    print("\n=======================")
+    print("PERFORMANCE METRICS")
+    print("=======================")
+
+    # Compute performance metrics
+    accuracy = np.mean(ytest == pred)
+    print("Accuracy: {0:d}/{1:d} ({2:0.1f}%)".format(sum(ytest == pred), num_testing_documents, accuracy * 100))
+    cm = confusion_matrix(ytest, pred)
+    print("Confusion matrix:")
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in cm]))
+
+    # pdb.set_trace()
 
 
 if __name__ == '__main__':
